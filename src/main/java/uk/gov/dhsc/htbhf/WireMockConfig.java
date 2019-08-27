45c738e66a29e34391d6eaf38abd315d2331f328
David Wybourn <david.wybourn@gmail.com>
2019-08-27 13:18:06 +0100

    HTBHF-1892 Added support to read mappings from classpath of spring boot jars.

--- src/main/java/uk/gov/dhsc/htbhf/WireMockConfig.java
+++ src/main/java/uk/gov/dhsc/htbhf/WireMockConfig.java
@@ -1,6 +1,7 @@
 package uk.gov.dhsc.htbhf;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.common.ClasspathFileSource;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +25,8 @@ public class WireMockConfig {
     private WireMockConfiguration wiremockOptions() {
         return options()
                 .port(port)
-                .usingFilesUnderDirectory("src/main/resources");
+                // work around for wiremock not reading from classpath of spring boot jars.
+                // see https://github.com/tomakehurst/wiremock/issues/725
+                .fileSource(new ClasspathFileSource("BOOT-INF/classes"));
     }
 }
